d65771a156ff98209b3963aeee786b8b7c933aed
JangKunil <jangkeya@gmail.com>
2023-02-05 17:16:23 +0900

    Add SenderCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/producer/SenderCore.java
@@ -0,0 +1,36 @@
+package com.github.hotire.springkafka.interview.producer;
+
+import java.util.List;
+import java.util.Set;
+
+import org.apache.kafka.clients.producer.internals.ProducerBatch;
+import org.apache.kafka.clients.producer.internals.Sender;
+import org.apache.kafka.common.Cluster;
+
+/**
+ * @see org.apache.kafka.clients.producer.internals.Sender
+ */
+public class SenderCore {
+
+    /**
+     * @see Sender#runOnce()
+     */
+    void runOnce() {
+
+    }
+
+    /**
+     * @see org.apache.kafka.clients.producer.internals.Sender#sendProducerData(long)
+     * @see org.apache.kafka.clients.producer.internals.RecordAccumulator#drain(Cluster, Set, int, long)
+     */
+    private long sendProducerData(long now) {
+        return now;
+    }
+
+    /**
+     * @see org.apache.kafka.clients.producer.internals.Sender#sendProduceRequest(long, int, short, int, List)
+     */
+    private void sendProduceRequest(long now, int destination, short acks, int timeout, List<ProducerBatch> batches) {
+
+    }
+}
