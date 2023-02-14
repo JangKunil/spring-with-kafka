5b3eeeacc6a9249c69b94bd135353e7c43d54dc1
JangKunil <jangkeya@gmail.com>
2023-02-14 21:50:07 +0900

    add RequestFutureCompletion Handler Core

---
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureCompletionHandlerCore.java
@@ -0,0 +1,25 @@
+package com.github.hotire.springkafka.interview.consumer;
+
+import org.apache.kafka.clients.ClientResponse;
+import org.apache.kafka.clients.RequestCompletionHandler;
+
+/**
+ * @see org.apache.kafka.clients.consumer.internals.ConsumerNetworkClient.RequestFutureCompletionHandler
+ */
+class RequestFutureCompletionHandlerCore implements RequestCompletionHandler {
+
+    /**
+     * @see org.apache.kafka.clients.consumer.internals.ConsumerNetworkClient.RequestFutureCompletionHandler#onComplete(ClientResponse)
+     */
+    @Override
+    public void onComplete(ClientResponse response) {
+
+    }
+
+    /**
+     * @see org.apache.kafka.clients.consumer.internals.ConsumerNetworkClient.RequestFutureCompletionHandler#onFailure(RuntimeException)
+     */
+    public void onFailure(RuntimeException e) {
+
+    }
+}
