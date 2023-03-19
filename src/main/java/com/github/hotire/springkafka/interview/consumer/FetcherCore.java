72f7a26caef9bfe04c7363a66cb4f44f3094982a
JangKunil <jangkeya@gmail.com>
2023-03-19 11:36:10 +0900

    Add FetcherCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/FetcherCore.java
@@ -0,0 +1,30 @@
+package com.github.hotire.springkafka.interview.consumer;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.internals.Fetcher;
+import org.apache.kafka.common.TopicPartition;
+
+/**
+ * @see Fetcher
+ * @see org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_RECORDS_CONFIG
+ */
+public class FetcherCore<K, V> {
+
+    /**
+     * @see Fetcher#fetchedRecords()
+     */
+    public Map<TopicPartition, List<ConsumerRecord<K, V>>> fetchedRecords() {
+        return Collections.emptyMap();
+    }
+
+    /**
+     * @see Fetcher#sendFetches()
+     */
+    public synchronized int sendFetches() {
+        return 0;
+    }
+}
