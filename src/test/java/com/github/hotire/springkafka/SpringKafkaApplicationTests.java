9df80d31a777db8fecb3edc697fc2d7966fe3ba5
JangKunil <jangkeya@gmail.com>
2024-04-05 23:48:15 +0900

    change SpringKafkaApplicationTests

--- src/test/java/com/github/hotire/springkafka/SpringKafkaApplicationTests.java
+++ src/test/java/com/github/hotire/springkafka/SpringKafkaApplicationTests.java
@@ -15,26 +15,30 @@ import com.github.hotire.springkafka.getting_started.producer.Sender;
 
 @DirtiesContext
 @EmbeddedKafka(partitions = 1,
-               topics = { SpringKafkaApplicationTests.HELLOWORLD_TOPIC })
+               topics = { SpringKafkaApplicationTests.HELLOWORLD_TOPIC },
+               brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" }
+)
 @SpringBootTest
 class SpringKafkaApplicationTests {
 
-  static final String HELLOWORLD_TOPIC = "helloworld.t";
+    static final String HELLOWORLD_TOPIC = "helloworld.t";
 
-  @Autowired
-  private Receiver receiver;
+    @Autowired
+    private Receiver receiver;
 
-  @Autowired
-  private Sender sender;
+    @Autowired
+    private Sender sender;
 
-  @Test
-  void testReceive() throws Exception {
-    final String message = "Hello Spring Kafka!";
-    sender.send(message);
-    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-    receiver.getLatch().await();
-    assertThat(receiver.getLatch().getCount()).isEqualTo(0);
-    assertThat(receiver.getMessages()).contains(message);
-  }
+    @Test
+    void testReceive() throws Exception {
+        final String message = "Hello Spring Kafka!";
+//        sender.send(message);
+        sender.send(message);
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        receiver.getLatch().await();
+        assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+//    assertThat(receiver.getMessages()).contains(message);
+        Thread.sleep(40000L);
+    }
 
 }
