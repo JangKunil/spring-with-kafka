c8e54082f66055ac9db29ae29690d8a0b6ed247a
JangKunil <jangkeya@gmail.com>
2024-04-19 00:10:06 +0900

    study MessageListenerContainerCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/MessageListenerContainerCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springkafka.core.consumer;
+
+/**
+ * @see org.springframework.kafka.listener.MessageListenerContainer
+ * @see org.springframework.kafka.listener.AbstractMessageListenerContainer
+ * @see org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+ */
+public class MessageListenerContainerCore {
+}
